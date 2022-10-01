0a4b77834fd6cf4a02999b3eadc7ccb18c550a9f
Paulo Ferreira <phgferreira@outlook.com>
2022-09-30 22:40:25 -0300

    Atualizando dados de vendedor e produto

--- script.sql
+++ script.sql
@@ -30,6 +30,8 @@ insert into sucos.vendedor (matricula, nome, percentual_comissao) values
 ('00233', 'João Geraldo da Fonseca', 0.10),
 ('00235', 'Márcio Almeida Silva', 0.08),
 ('00236', 'Cláudia Morais', 0.08);
+update sucos.vendedor set percentual_comissao = 0.11 where matricula = '00236';
+update sucos.vendedor set nome = 'José Geraldo da Fonseca Junior' where matricula = '00233';
 select * from sucos.vendedor;
 
 -- feito pelo assistente do MySQL Workbench
@@ -44,5 +46,9 @@ insert into sucos.produto (id, nome, embalagem, tamanho, sabor, preco) values
 ('1040107', 'Light - 350 ml - Melância', 'Lata', '350 ml', 'Melância', 4.56),
 ('1037797', 'Clean - 2 Litros - Laranja', 'PET', '2 Litros', 'Laranja', 16.01),
 ('1000889', 'Sabor da Montanha - 700 ml', 'Garrafa', '700 ml', 'Uva', 6.31),
-('1004327', 'Videira do Campo - 1,5 Litros - Malância', 'PET', '1,5 Litros', 'Melância', 19.51);
+('1004327', 'Videira do Campo - 1,5 Litros - Malância', 'PET', '1,5 Litros', 'Melância', 19.51),
+('544931', 'Frescor do Verão - 350 ml - Limão', 'PET', '350 ml','Limão',3.20),
+('1078680', 'Frescor do Verão - 470 ml - Manga', 'Lata', '470 ml','Manga',5.18);
+update sucos.produto set embalagem = 'Lata', preco = 2.46 where id = '544931';
+update sucos.produto set embalagem = 'Garrafa' where id = '1078680';
 select * from sucos.produto;
