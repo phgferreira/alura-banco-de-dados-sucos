364ca7f5fb8aa188f04d9de5843a205b18e9a013
Paulo Ferreira <phgferreira@outlook.com>
2022-09-30 22:03:10 -0300

    Inseridos novos produtos e novos vendedores

--- script.sql
+++ script.sql
@@ -26,8 +26,11 @@ create table if not exists sucos.vendedor (
 );
 create table sucos.vendedor2 as select * from sucos.vendedor;
 drop table sucos.vendedor2;
-insert into sucos.vendedor (matricula, nome, percentual_comissao)
-values ('00233', 'João Geraldo da Fonseca', 0.10);
+insert into sucos.vendedor (matricula, nome, percentual_comissao) values
+('00233', 'João Geraldo da Fonseca', 0.10),
+('00235', 'Márcio Almeida Silva', 0.08),
+('00236', 'Cláudia Morais', 0.08);
+select * from sucos.vendedor;
 
 -- feito pelo assistente do MySQL Workbench
 CREATE TABLE `sucos`.`produto` (
@@ -37,6 +40,9 @@ CREATE TABLE `sucos`.`produto` (
   `tamanho` VARCHAR(50) NULL,
   `sabor` VARCHAR(50) NULL,
   `preco` FLOAT NULL);
-insert into sucos.produto (id, nome, embalagem, tamanho, sabor, preco)
-values ('1040107', 'Light - 350 ml - Melância', 'Lata', '350 ml', 'Melância', 4.56);
+insert into sucos.produto (id, nome, embalagem, tamanho, sabor, preco) values
+('1040107', 'Light - 350 ml - Melância', 'Lata', '350 ml', 'Melância', 4.56),
+('1037797', 'Clean - 2 Litros - Laranja', 'PET', '2 Litros', 'Laranja', 16.01),
+('1000889', 'Sabor da Montanha - 700 ml', 'Garrafa', '700 ml', 'Uva', 6.31),
+('1004327', 'Videira do Campo - 1,5 Litros - Malância', 'PET', '1,5 Litros', 'Melância', 19.51);
 select * from sucos.produto;
