4a7f59ff6fcde9f0710ec5bfe91d08abf4730f6d
Paulo Ferreira <phgferreira@outlook.com>
2022-10-02 11:11:50 -0300

    Filtro de datas

--- queries.sql
+++ queries.sql
@@ -30,3 +30,8 @@ select * from tbproduto where preco_lista = 16.008;
 
 select * from tbproduto where preco_lista between 16.007 and 16.009;
 
+select * from tbcliente where data_nascimento <= '1995-01-13';
+
+select * from tbcliente where year(data_nascimento) = 1995;
+
+select * from tbcliente where month(data_nascimento) = 10;
