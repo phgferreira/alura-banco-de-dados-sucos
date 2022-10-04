5b25199da886989e8f3f7fab798bb8ad75236c0f
Paulo Ferreira <phgferreira@outlook.com>
2022-10-03 21:35:48 -0300

    Usando um pouco mais dos filtros compostos

--- modulo2/queries.sql
+++ modulo2/queries.sql
@@ -9,3 +9,15 @@ select * from tabela_de_produtos where sabor = 'Laranja';
 select * from tabela_de_produtos where embalagem = 'pet';
 
 select * from tabela_de_produtos where preco_de_lista between 19.51 and 19.52;
+
+select * from tabela_de_produtos where sabor = 'manga' and tamanho = '470 ml';
+
+select * from tabela_de_produtos where not(sabor = 'manga' or tamanho = '470 ml');
+
+select * from tabela_de_produtos where sabor in ('laranja','manga');
+
+select * from tabela_de_clientes where cidade in ('rio de janeiro','são paulo') and idade >= 20;
+
+select * from tabela_de_clientes where cidade in ('rio de janeiro','são paulo') and idade between 19 and 22;
+
+
