ca17038260baaeedf42fad80550e7d4b4c078881
Paulo Ferreira <phgferreira@outlook.com>
2022-10-03 21:08:14 -0300

    Renomeado o nome do arquivo sql e feito as primeiras consultas (para relembrar conteúdo de filtros)

---
+++ modulo2/queries.sql
@@ -0,0 +1,11 @@
+select cpf identificador, nome cliente from tabela_de_clientes;
+
+select * from tabela_de_produtos where codigo_do_produto = '1000889';
+
+select * from tabela_de_produtos where sabor = 'Uva';
+
+select * from tabela_de_produtos where sabor = 'Laranja';
+
+select * from tabela_de_produtos where embalagem = 'pet';
+
+select * from tabela_de_produtos where preco_de_lista between 19.51 and 19.52;
