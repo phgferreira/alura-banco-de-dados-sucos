30d70244cd55be65c23b5167a102809301e5fac8
Paulo Ferreira <phgferreira@outlook.com>
2022-10-05 20:37:38 -0300

    Realizando consultas agrupadas

--- modulo2/queries.sql
+++ modulo2/queries.sql
@@ -45,3 +45,26 @@ select * from tabela_de_produtos order by nome_do_produto;
 select * from tabela_de_produtos order by nome_do_produto desc;
 
 select * from tabela_de_produtos order by embalagem desc, nome_do_produto;
+
+select * from tabela_de_clientes;
+
+select estado, limite_de_credito from tabela_de_clientes;
+
+select estado, sum(limite_de_credito) limite_total from tabela_de_clientes group  by estado;
+
+select embalagem, preco_de_lista from tabela_de_produtos;
+
+select embalagem, max(preco_de_lista) maior_preco from tabela_de_produtos group by embalagem;
+
+select embalagem, count(*) contador from tabela_de_produtos group by embalagem;
+
+select bairro, sum(limite_de_credito) limite from tabela_de_clientes where cidade = 'Rio de Janeiro' group by bairro;
+
+select estado, bairro, sum(limite_de_credito) limite
+from  tabela_de_clientes
+where cidade = 'Rio de Janeiro'
+group by estado, bairro
+order by bairro;
+
+
+
