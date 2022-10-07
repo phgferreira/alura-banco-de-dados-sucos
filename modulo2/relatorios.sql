903298981f56f8b2ddb5c902de1eba7e314f75b8
Paulo Ferreira <phgferreira@outlook.com>
2022-10-06 21:26:53 -0300

    Relatório de quantidade e participação de vendas por sabor no ano de 2016

--- modulo2/relatorios.sql
+++ modulo2/relatorios.sql
@@ -16,3 +16,24 @@ inner join	tabela_de_clientes c 		on c.cpf = nf.cpf
 group by date_format(data_venda,'%m/%Y'), c.cpf, c.nome
 /*having sum(quantidade) <= c.volume_de_compra*/
 order by soma_quantidade desc;
+
+/* --------------------------------------------------------------- */
+/* Fazer acompanhamento sobra as vendas do ano de 2016 por sabores */
+
+with total as (
+	select sum(inf.quantidade) qt
+	from		itens_notas_fiscais inf
+	inner join	notas_fiscais nf			on nf.numero = inf.numero
+	where year(nf.data_venda) = 2016
+)
+
+select p.sabor, year(nf.data_venda) ano,
+	sum(inf.quantidade) quantidade,
+    round( ( sum(inf.quantidade) / (select qt from total) )*100, 2) participacao
+from		itens_notas_fiscais inf
+inner join	notas_fiscais nf			on nf.numero = inf.numero
+inner join	tabela_de_produtos p		on p.codigo_do_produto = inf.codigo_do_produto
+where year(nf.data_venda) = 2016
+group by sabor, year(nf.data_venda)
+order by participacao desc;
+
